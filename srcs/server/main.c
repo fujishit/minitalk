d805883cc02808814220c627001b4b0bba577a07
mfujishi <mfujishi@student.42tokyo.jp>
2021-06-01 03:57:52 +0900

    server tabun kansei

--- srcs/server/main.c
+++ srcs/server/main.c
@@ -7,17 +7,63 @@
 // 	int			sa_flags;
 // 	void		(*sa_restorer)(void);
 // };
+int		server_sigtochar(char *c)
+{
+	int	ret;
+	int	i;
+
+	ret = 0;
+	ret += (128 * (c[0] - '0'));
+	ret += (64 * (c[1] - '0'));
+	ret += (32 * (c[2] - '0'));
+	ret += (16 * (c[3] - '0'));
+	ret += (8 * (c[4] - '0'));
+	ret += (4 * (c[5] - '0'));
+	ret += (2 * (c[6] - '0'));
+	ret += (1 * (c[7] - '0'));
+	return (ret);
+}
+
+void	server_output(siginfo_t *siginfo, char *c)
+{
+	int	ascii;
+
+	ascii = server_sigtochar(c);
+	write(1, &ascii, 1);
+}
+
+void	server_check(int sig, siginfo_t *siginfo, void *idk)
+{
+	static char	c[8];
+	static int	num = 0;
+
+	(void)idk;
+	if (sig == SIGUSR1)
+		c[num] = '0';
+	else if (sig == SIGUSR2)
+		c[num] = '1';
+	num++;
+	if (num == 8)
+	{
+		num = 0;
+		server_output(siginfo, c);
+		ft_bzero(c, sizeof(c) * 8);
+	}
+}
 
 int	main(void)
 {
-	//struct sigaction	sig;
-	pid_t	pid;
+	struct sigaction	sig;
 
-	//sigemptyset(&sig.sa_mask);
-	pid = getpid();
-	write(1, "pid:", 4);
-	ft_putnbr_fd(pid, 1);
+	ft_putnbr_fd(getpid(), 2);
 	write(1, "\n", 1);
-	pause();
-	write(1, "ポーズ終了\n", 6);
+	sigemptyset(&sig.sa_mask);
+	sig.sa_flags = SA_SIGINFO;
+	sig.sa_sigaction = server_check;
+	if (sigaction(SIGUSR1, &sig, NULL) == -1)
+		return (0);
+	else if (sigaction(SIGUSR2, &sig, NULL) == -1)
+		return (0);
+	while (1)
+		pause();
 }
